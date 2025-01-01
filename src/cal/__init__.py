49300d6d4b0a0c2f1344f2b8923e99ae9805c275
nuuuwan <nuuuwan@gmail.com>
2025-01-01 09:54:35 +0530

    Deprecate CalendarEvent

--- src/cal/__init__.py
+++ src/cal/__init__.py
@@ -1,6 +1,6 @@
 # cal (auto generate by build_inits.py)
 
-from cal.CalendarEvent import CalendarEvent
+from cal.AbstractCalendar import AbstractCalendar
 from cal.HOLIDAYS import HOLIDAYS
 from cal.MonthCalendar import MonthCalendar
 from cal.WeekCalendar import WeekCalendar
