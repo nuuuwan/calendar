a92b1bd96c34f26e0b001e9cdf122006306b3255
nuuuwan <nuuuwan@gmail.com>
2025-03-30 14:39:37 +0530

    Various

--- workflows/build_calendars.py
+++ workflows/build_calendars.py
@@ -3,8 +3,8 @@ from cal import MonthCalendar, WeekCalendar
 if __name__ == "__main__":
 
     year = 2025
-    for month in [2, 3, 4]:
+    for month in [5]:
         MonthCalendar(year, month).draw()
 
-    for month, day in [[2, 10], [2, 17], [2, 24]]:
+    for month, day in [[3, 31], [4,7]]:
         WeekCalendar(year, month, day).draw()
