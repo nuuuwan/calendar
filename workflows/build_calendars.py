818cbf9c82a4da4ff8247bb4bb50bbc05924d669
nuuuwan <nuuuwan@gmail.com>
2025-05-03 08:25:12 +0530

    New ranges

--- workflows/build_calendars.py
+++ workflows/build_calendars.py
@@ -3,8 +3,8 @@ from cal import MonthCalendar, WeekCalendar
 if __name__ == "__main__":
 
     year = 2025
-    for month in [5]:
+    for month in [6, 7, 8]:
         MonthCalendar(year, month).draw()
 
-    for month, day in [[3, 31], [4,7]]:
+    for month, day in [[5, 5], [5, 12], [5, 19]]:
         WeekCalendar(year, month, day).draw()
